a2166f3e7cbc9a7dd7cecf13bd95dca24a2ee311
JoyHong <hongwanli163@gmail.com>
2015-09-08 17:35:46 +0800

    Cookies管理机制
    Cookies本地存储机制

--- HGHttpCookiesMaster/HGCookiesWebVC.h
+++ HGHttpCookiesMaster/HGCookiesWebVC.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  HGCookiesWebVC.h
 //  HGHttpCookiesMaster
 //
 //  Created by XiaoDou on 15/8/24.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface HGCookiesWebVC : UIViewController
 
 @end
-
---
+++ HGHttpCookiesMaster/HGCookiesWebVC.m
@@ -0,0 +1,114 @@
+//
+//  HGCookiesWebVC.m
+//  HGHttpCookiesMaster
+//
+//  Created by XiaoDou on 15/8/24.
+//  Copyright (c) 2015年 北京嗨购电子商务有限公司. All rights reserved.
+//
+
+#import "HGCookiesWebVC.h"
+
+#define kSystemCookies              @"kSystemCookies"
+
+@interface HGCookiesWebVC () <UIWebViewDelegate>
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+
+@end
+
+@implementation HGCookiesWebVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.title = @"Cookies测试页";
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleHttpCookiesDidChanged:) name:NSHTTPCookieManagerCookiesChangedNotification object:nil];
+//    [self outPutAllCookies];
+    NSURL *URL = [NSURL URLWithString:@"http://www.weibo.com"];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:URL];
+//    [self loadHttpRequestCookiesIfNeeded];
+    [_webView loadRequest:request];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    [_webView stopLoading];
+    _webView.delegate = nil;
+//    [self deleteAllCookies];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)dealloc {
+
+}
+
+#pragma mark -UIWebViewDelegate
+
+- (void)webViewDidStartLoad:(UIWebView *)webView {
+
+}
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView {
+    [self saveHttpRequestCookies];
+}
+
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
+}
+
+#pragma mark - Notification
+
+- (void)handleHttpCookiesDidChanged:(NSNotification *)notification {
+    NSLog(@"handleHttpCookiesDidChanged");
+    [self outPutAllCookies];
+}
+
+#pragma mark - About Cookie
+
+- (void)outPutAllCookies {
+    NSArray *cookies = [[NSHTTPCookieStorage sharedHTTPCookieStorage] cookies];
+    NSHTTPCookie *cookie = nil;
+    for (id cObj in cookies) {
+        if ([cObj isKindOfClass:[NSHTTPCookie class]]){
+            cookie = (NSHTTPCookie *)cObj;
+            NSLog(@"cookie properties: %@", cookie.properties);
+        }
+    }
+}
+
+- (void)saveHttpRequestCookies {
+    NSArray *cookies = [[NSHTTPCookieStorage sharedHTTPCookieStorage] cookies];
+    NSString *filePath = [self getCookiesFilePath];
+    [NSKeyedArchiver archiveRootObject:cookies toFile:filePath];
+}
+
+- (void)loadHttpRequestCookiesIfNeeded {
+    NSString *filePath = [self getCookiesFilePath];
+    NSArray *cookies = [NSKeyedUnarchiver unarchiveObjectWithFile:filePath];
+    NSHTTPCookie *cookie = nil;
+    for (cookie in cookies) {
+        [[NSHTTPCookieStorage sharedHTTPCookieStorage] setCookie:cookie];
+    }
+}
+
+- (void)deleteAllCookies {
+    NSString *filePath = [self getCookiesFilePath];
+    NSArray *cookies = [NSKeyedUnarchiver unarchiveObjectWithFile:filePath];
+    NSHTTPCookie *cookie = nil;
+    for (cookie in cookies) {
+        [[NSHTTPCookieStorage sharedHTTPCookieStorage] deleteCookie:cookie];
+    }
+    NSFileManager *fileManager = [[NSFileManager alloc] init];
+    NSError *error = nil;
+    [fileManager removeItemAtPath:filePath error:&error];
+}
+
+- (NSString *)getCookiesFilePath {
+    NSString *documentPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+    NSString *filePath = [documentPath stringByAppendingPathComponent:kSystemCookies];
+    return filePath;
+}
+
+@end
